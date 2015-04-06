bdde6448cf001e23ecf634e4e208aa85e382ffd7
Steven Chim <stevenchim@gmail.com>
2015-04-06 21:48:06 +0200

    added more tests

--- test/http-proxy-middleware.spec.js
+++ test/http-proxy-middleware.spec.js
@@ -2,13 +2,30 @@ var expect          = require('chai').expect;
 var proxyMiddleware = require('../index');
 
 describe('http-proxy-middleware', function () {
+    var middleware;
 
     it('should be a function', function () {
         expect(proxyMiddleware).to.be.a('function');
     });
 
-    it('should create a new proxy', function () {
-        proxyMiddleware('/api', {target:'localhost:9000'});
+    it('should create a proxy middleware', function () {
+        middleware = proxyMiddleware('/api', {target:'localhost:9000'});
+        expect(middleware).to.be.a('function');
+    });
+
+    it('should not proxy requests when request url does not match middleware context' , function () {
+        var skipped = false;
+
+        var mockReq = {url:'/foo/bar'};
+        var mockRes = {};
+        var mockNext = function () {
+            // mockNext will be called when request is not proxied
+            skipped = true;
+        };
+
+        middleware = proxyMiddleware('/api', {target:'localhost:9000'});
+        middleware(mockReq, mockRes, mockNext);
+        expect(skipped).to.be.true;
     });
 
 });
