adf97365b40a34bfa40a46cbcd1fa122141720f7
Michael Stramel <m.stramel89@gmail.com>
2016-10-11 02:40:30 -0500

    feat(router): Add ability for router function to resolve to a proxyTable

--- test/e2e/router.spec.js
+++ test/e2e/router.spec.js
@@ -35,31 +35,97 @@ describe('E2E router', function() {
         targetServerC.close();
     });
 
-    describe('router with proxyTable', function() {
-        beforeEach(function() {
-            proxyServer = createServer(6000, proxyMiddleware({
-                target: 'http://localhost:6001',
-                router: function(req) {
-                    return 'http://localhost:6003';
-                }
-            }));
-        });
+    describe('router with proxy function', function() {
+        describe('that returns string', function () {
+            beforeEach(function() {
+                proxyServer = createServer(6000, proxyMiddleware({
+                    target: 'http://localhost:6001',
+                    router: function(req) {
+                        return 'http://localhost:6003';
+                    }
+                }));
+            });
 
-        afterEach(function() {
-            proxyServer.close();
-        });
+            afterEach(function() {
+                proxyServer.close();
+            });
 
-        it('should proxy to: "localhost:6003/api"', function(done) {
-            var options = {hostname: 'localhost', port: 6000, path: '/api'};
-            http.get(options, function(res) {
-                res.on('data', function(chunk) {
-                    var responseBody = chunk.toString();
-                    expect(responseBody).to.equal('C');
-                    done();
+            it('should proxy to: "localhost:6003/api"', function(done) {
+                var options = {hostname: 'localhost', port: 6000, path: '/api'};
+                http.get(options, function(res) {
+                    res.on('data', function(chunk) {
+                        var responseBody = chunk.toString();
+                        expect(responseBody).to.equal('C');
+                        done();
+                    });
                 });
             });
         });
 
+        describe('that returns proxyTable', function () {
+            beforeEach(function setupServers() {
+                proxyServer = createServer(6000, proxyMiddleware('/', {
+                    target: 'http://localhost:6001',
+                    router: function(req) {
+                        return {
+                            'alpha.localhost:6000': 'http://localhost:6001',
+                            'beta.localhost:6000': 'http://localhost:6002',
+                            'localhost:6000/api': 'http://localhost:6003'
+                        };
+                    }
+                }));
+
+            });
+
+            afterEach(function() {
+                proxyServer.close();
+            });
+
+            it('should proxy to option.target', function(done) {
+                http.get('http://localhost:6000', function(res) {
+                    res.on('data', function(chunk) {
+                        var responseBody = chunk.toString();
+                        expect(responseBody).to.equal('A');
+                        done();
+                    });
+                });
+            });
+
+            it('should proxy when host is "alpha.localhost"', function(done) {
+                var options = {hostname: 'localhost', port: 6000, path: '/'};
+                options.headers = {host: 'alpha.localhost:6000'};
+                http.get(options, function(res) {
+                    res.on('data', function(chunk) {
+                        var responseBody = chunk.toString();
+                        expect(responseBody).to.equal('A');
+                        done();
+                    });
+                });
+            });
+
+            it('should proxy when host is "beta.localhost"', function(done) {
+                var options = {hostname: 'localhost', port: 6000, path: '/'};
+                options.headers = {host: 'beta.localhost:6000'};
+                http.get(options, function(res) {
+                    res.on('data', function(chunk) {
+                        var responseBody = chunk.toString();
+                        expect(responseBody).to.equal('B');
+                        done();
+                    });
+                });
+            });
+
+            it('should proxy with host & path config: "localhost:6000/api"', function(done) {
+                var options = {hostname: 'localhost', port: 6000, path: '/api'};
+                http.get(options, function(res) {
+                    res.on('data', function(chunk) {
+                        var responseBody = chunk.toString();
+                        expect(responseBody).to.equal('C');
+                        done();
+                    });
+                });
+            });
+        });
     });
 
     describe('router with proxyTable', function() {
--- test/unit/router.spec.js
+++ test/unit/router.spec.js
@@ -21,27 +21,125 @@ describe('router unit test', function() {
     describe('router.getTarget from function', function() {
         var request;
 
-        beforeEach(function() {
-            proxyOptionWithRouter = {
-                target: 'http://localhost:6000',
-                router: function(req) {
-                    request = req;
-                    return 'http://foobar.com:666';
-                }
-            };
+        describe('returning string', function () {
+            beforeEach(function() {
+                proxyOptionWithRouter = {
+                    target: 'http://localhost:6000',
+                    router: function(req) {
+                        request = req;
+                        return 'http://foobar.com:666';
+                    }
+                };
 
-            result = router.getTarget(req, proxyOptionWithRouter);
+                result = router.getTarget(req, proxyOptionWithRouter);
+            });
+
+            describe('custom dynamic router function', function() {
+                it('should provide the request object for dynamic routing', function() {
+                    expect(request.headers.host).to.equal('localhost');
+                    expect(request.url).to.equal('/');
+                });
+                it('should return new target', function() {
+                    expect(result).to.equal('http://foobar.com:666');
+                });
+            });
         });
 
-        describe('custom dynamic router function', function() {
-            it('should provide the request object for dynamic routing', function() {
-                expect(request.headers.host).to.equal('localhost');
-                expect(request.url).to.equal('/');
+        describe('returning table', function () {
+            beforeEach(function() {
+                proxyOptionWithRouter = {
+                    target: 'http://localhost:6000',
+                    router: function (req) {
+                        request = req;
+                        return {
+                            'alpha.localhost': 'http://localhost:6001',
+                            'beta.localhost': 'http://localhost:6002',
+                            'gamma.localhost/api': 'http://localhost:6003',
+                            'gamma.localhost': 'http://localhost:6004',
+                            '/rest': 'http://localhost:6005',
+                            '/some/specific/path': 'http://localhost:6006',
+                            '/some': 'http://localhost:6007'
+                        };
+                    }
+                };
             });
-            it('should return new target', function() {
-                expect(result).to.equal('http://foobar.com:666');
+
+            describe('without router config', function() {
+                it('should return the normal target when router not present in config', function() {
+                    result = router.getTarget(req, config);
+                    expect(result).to.equal(undefined);
+                });
+            });
+
+            describe('with just the host in router config', function() {
+                it('should provide the request object for dynamic routing', function() {
+                    expect(request.headers.host).to.equal('localhost');
+                    expect(request.url).to.equal('/');
+                });
+                it('should target http://localhost:6001 when for router:"alpha.localhost"', function() {
+                    req.headers.host = 'alpha.localhost';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6001');
+                });
+
+                it('should target http://localhost:6002 when for router:"beta.localhost"', function() {
+                    req.headers.host = 'beta.localhost';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6002');
+                });
+            });
+
+            describe('with host and host + path config', function() {
+                it('should target http://localhost:6004 without path', function() {
+                    req.headers.host = 'gamma.localhost';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6004');
+                });
+
+                it('should target http://localhost:6003 exact path match', function() {
+                    req.headers.host = 'gamma.localhost';
+                    req.url = '/api';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6003');
+                });
+
+                it('should target http://localhost:6004 when contains path', function() {
+                    req.headers.host = 'gamma.localhost';
+                    req.url = '/api/books/123';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6003');
+                });
+            });
+
+            describe('with just the path', function() {
+                it('should target http://localhost:6005 with just a path as router config', function() {
+                    req.url = '/rest';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6005');
+                });
+
+                it('should target http://localhost:6005 with just a path as router config', function() {
+                    req.url = '/rest/deep/path';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6005');
+                });
+
+                it('should target http://localhost:6000 path in not present in router config', function() {
+                    req.url = '/unknow-path';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal(undefined);
+                });
+            });
+
+            describe('matching order of router config', function() {
+                it('should return first matching target when similar paths are configured', function() {
+                    req.url = '/some/specific/path';
+                    result = router.getTarget(req, proxyOptionWithRouter);
+                    expect(result).to.equal('http://localhost:6006');
+                });
             });
         });
+
     });
 
     describe('router.getTarget from table', function() {
