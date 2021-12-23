0fbf74b52d3e5beb2f1bd8caf4d80910bacceaee
chimurai <655241+chimurai@users.noreply.github.com>
2021-12-23 17:46:23 +0000

    build(eslint): disable @typescript-eslint/no-explicit-any

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       { vars: 'all', args: 'none', ignoreRestSiblings: false },
