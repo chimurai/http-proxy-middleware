7a3c6d424ec19ec0744d947ff029e811f8b177d8
chimurai <655241+chimurai@users.noreply.github.com>
2022-05-05 22:17:19 +0200

    refactor(package): remove optional @types/express

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,11 @@ module.exports = {
         '@typescript-eslint/no-var-requires': 'off',
       },
     },
+    {
+      files: ['src/**/*.ts'],
+      rules: {
+        'no-restricted-imports': ['error', { paths: ['express'] }],
+      },
+    },
   ],
 };
