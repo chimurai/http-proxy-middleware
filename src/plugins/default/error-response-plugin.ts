d9d667864b4d759e0cbb2b1cbe1828579913fa30
Nathan Walters <nwalters512@gmail.com>
2022-09-04 17:18:47 -0700

    fix(websocket): handle sockets in error-response-plugin

--- src/plugins/default/error-response-plugin.ts
+++ src/plugins/default/error-response-plugin.ts
@@ -1,19 +1,32 @@
+import type { Socket } from 'net';
 import { getStatusCode } from '../../status-code';
 import { Plugin, Response } from '../../types';
 
+function isResponseLike(obj: any): obj is Response {
+  return obj && typeof obj.writeHead === 'function';
+}
+
+function isSocketLike(obj: any): obj is Socket {
+  return obj && typeof obj.write === 'function' && !('writeHead' in obj);
+}
+
 export const errorResponsePlugin: Plugin = (proxyServer, options) => {
-  proxyServer.on('error', (err, req, res: Response, target?) => {
+  proxyServer.on('error', (err, req, res: Response | Socket, target?) => {
     // Re-throw error. Not recoverable since req & res are empty.
     if (!req && !res) {
       throw err; // "Error: Must provide a proper URL as target"
     }
 
-    if (res.writeHead && !res.headersSent) {
-      const statusCode = getStatusCode((err as unknown as any).code);
-      res.writeHead(statusCode);
-    }
+    if (isResponseLike(res)) {
+      if (!res.headersSent) {
+        const statusCode = getStatusCode((err as unknown as any).code);
+        res.writeHead(statusCode);
+      }
 
-    const host = req.headers && req.headers.host;
-    res.end(`Error occurred while trying to proxy: ${host}${req.url}`);
+      const host = req.headers && req.headers.host;
+      res.end(`Error occurred while trying to proxy: ${host}${req.url}`);
+    } else if (isSocketLike(res)) {
+      res.destroy();
+    }
   });
 };
