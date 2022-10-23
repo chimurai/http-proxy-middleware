694c5ee43492702c38709b074289658fb1ce2297
igorr <igor.razutkin@list.ru>
2022-10-23 10:39:50 +0300

    feat: ability to change proxy options for any request

--- src/http-proxy-middleware.ts
+++ src/http-proxy-middleware.ts
@@ -51,6 +51,9 @@ export class HttpProxyMiddleware {
     if (this.shouldProxy(this.config.context, req)) {
       try {
         const activeProxyOptions = await this.prepareProxyRequest(req);
+        if (this.proxyOptions.changeProxyOptions) {
+          this.proxyOptions.changeProxyOptions(activeProxyOptions);
+        }
         this.proxy.web(req, res, activeProxyOptions);
       } catch (err) {
         next(err);
