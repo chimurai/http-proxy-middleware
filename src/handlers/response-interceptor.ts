291d388b397254ad7a27f7577f92060566285a78
gauravpant1306 <45787597+gauravpant1306@users.noreply.github.com>
2022-11-21 22:12:52 +0530

    Update response-interceptor.ts

--- src/handlers/response-interceptor.ts
+++ src/handlers/response-interceptor.ts
@@ -1,17 +1,8 @@
-import type * as http from 'http';
-import * as zlib from 'zlib';
-import { Debug } from '../debug';
-import { getFunctionName } from '../utils/function';
-
-const debug = Debug.extend('response-interceptor');
-
-type Interceptor = (
-  buffer: Buffer,
-  proxyRes: http.IncomingMessage,
-  req: http.IncomingMessage,
-  res: http.ServerResponse
-) => Promise<Buffer | string>;
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.responseInterceptor = void 0;
+const zlib = require("zlib");
+const fs = require('fs');
 /**
  * Intercept responses from upstream.
  * Automatically decompress (deflate, gzip, brotli).
@@ -19,104 +10,90 @@ type Interceptor = (
  *
  * NOTE: must set options.selfHandleResponse=true (prevent automatic call of res.end())
  */
-export function responseInterceptor(interceptor: Interceptor) {
-  return async function proxyResResponseInterceptor(
-    proxyRes: http.IncomingMessage,
-    req: http.IncomingMessage,
-    res: http.ServerResponse
-  ): Promise<void> {
-    debug('intercept proxy response');
-    const originalProxyRes = proxyRes;
-    let buffer = Buffer.from('', 'utf8');
-
-    // decompress proxy response
-    const _proxyRes = decompress(proxyRes, proxyRes.headers['content-encoding']);
-
-    // concat data stream
-    _proxyRes.on('data', (chunk) => (buffer = Buffer.concat([buffer, chunk])));
-
-    _proxyRes.on('end', async () => {
-      // copy original headers
-      copyHeaders(proxyRes, res);
-
-      // call interceptor with intercepted response (buffer)
-      debug('call interceptor function: %s', getFunctionName(interceptor));
-      const interceptedBuffer = Buffer.from(await interceptor(buffer, originalProxyRes, req, res));
-
-      // set correct content-length (with double byte character support)
-      debug('set content-length: %s', Buffer.byteLength(interceptedBuffer, 'utf8'));
-      res.setHeader('content-length', Buffer.byteLength(interceptedBuffer, 'utf8'));
-
-      debug('write intercepted response');
-      res.write(interceptedBuffer);
-      res.end();
-    });
-
-    _proxyRes.on('error', (error) => {
-      res.end(`Error fetching proxied request: ${error.message}`);
-    });
-  };
+function responseInterceptor(interceptor) {
+    return async function proxyRes(proxyRes, req, res) {
+        const originalProxyRes = proxyRes;
+        let buffer = Buffer.from('', 'base64');
+        
+        let bufferArray = []
+        let state="free"
+        // decompress proxy response
+        const _proxyRes = decompress(proxyRes, proxyRes.headers['content-encoding']);
+        // concat data stream
+        _proxyRes.on('data',async (bufferChunk) =>{
+            bufferArray.push(bufferChunk)            
+        });
+        _proxyRes.on('end', async () => {
+            // copy original headers
+            copyHeaders(proxyRes, res);
+            // call interceptor with intercepted response (buffer)
+            let interceptedBuffer;
+            if(interceptor)
+            {
+                interceptedBuffer = await interceptor(bufferArray, originalProxyRes, req, res);
+            }
+            else
+            {
+                interceptedBuffer=buffer
+            }
+            res.setHeader('content-length', Buffer.byteLength(interceptedBuffer));
+            res.write(interceptedBuffer);
+            res.end();
+        });
+        _proxyRes.on('error', (error) => {
+            res.end(`Error fetching proxied request: ${error.message}`);
+        });
+    };
 }
-
+exports.responseInterceptor = responseInterceptor;
 /**
  * Streaming decompression of proxy response
  * source: https://github.com/apache/superset/blob/9773aba522e957ed9423045ca153219638a85d2f/superset-frontend/webpack.proxy-config.js#L116
  */
-function decompress(proxyRes: http.IncomingMessage, contentEncoding: string) {
-  let _proxyRes = proxyRes;
-  let decompress;
-
-  switch (contentEncoding) {
-    case 'gzip':
-      decompress = zlib.createGunzip();
-      break;
-    case 'br':
-      decompress = zlib.createBrotliDecompress();
-      break;
-    case 'deflate':
-      decompress = zlib.createInflate();
-      break;
-    default:
-      break;
-  }
-
-  if (decompress) {
-    debug(`decompress proxy response with 'content-encoding': %s`, contentEncoding);
-    _proxyRes.pipe(decompress);
-    _proxyRes = decompress;
-  }
-
-  return _proxyRes;
+function decompress(proxyRes, contentEncoding) {
+    let _proxyRes = proxyRes;
+    let decompress;
+    switch (contentEncoding) {
+        case 'gzip':
+            decompress = zlib.createGunzip();
+            break;
+        case 'br':
+            decompress = zlib.createBrotliDecompress();
+            break;
+        case 'deflate':
+            decompress = zlib.createInflate();
+            break;
+        default:
+            break;
+    }
+    if (decompress) {
+        _proxyRes.pipe(decompress);
+        _proxyRes = decompress;
+    }
+    return _proxyRes;
 }
-
 /**
  * Copy original headers
  * https://github.com/apache/superset/blob/9773aba522e957ed9423045ca153219638a85d2f/superset-frontend/webpack.proxy-config.js#L78
  */
 function copyHeaders(originalResponse, response) {
-  debug('copy original response headers');
-
-  response.statusCode = originalResponse.statusCode;
-  response.statusMessage = originalResponse.statusMessage;
-
-  if (response.setHeader) {
-    let keys = Object.keys(originalResponse.headers);
-
-    // ignore chunked, brotli, gzip, deflate headers
-    keys = keys.filter((key) => !['content-encoding', 'transfer-encoding'].includes(key));
-
-    keys.forEach((key) => {
-      let value = originalResponse.headers[key];
-
-      if (key === 'set-cookie') {
-        // remove cookie domain
-        value = Array.isArray(value) ? value : [value];
-        value = value.map((x) => x.replace(/Domain=[^;]+?/i, ''));
-      }
-
-      response.setHeader(key, value);
-    });
-  } else {
-    response.headers = originalResponse.headers;
-  }
+    response.statusCode = originalResponse.statusCode;
+    response.statusMessage = originalResponse.statusMessage;
+    if (response.setHeader) {
+        let keys = Object.keys(originalResponse.headers);
+        // ignore chunked, brotli, gzip, deflate headers
+        keys = keys.filter((key) => !['content-encoding', 'transfer-encoding'].includes(key));
+        keys.forEach((key) => {
+            let value = originalResponse.headers[key];
+            if (key === 'set-cookie') {
+                // remove cookie domain
+                value = Array.isArray(value) ? value : [value];
+                value = value.map((x) => x.replace(/Domain=[^;]+?/i, ''));
+            }
+            response.setHeader(key, value);
+        });
+    }
+    else {
+        response.headers = originalResponse.headers;
+    }
 }
