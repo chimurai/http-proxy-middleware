52c2c444efaecd43b14a0fc0c33ee28a343b0d81
The Mark <sunduo3195@qq.com>
2023-02-16 10:53:39 +0800

    fix(utils): add unit test and split func

--- src/handlers/fix-request-body.ts
+++ src/handlers/fix-request-body.ts
@@ -4,6 +4,21 @@ import type { Request } from '../types';
 import * as querystring from 'querystring';
 
 /**
+ * format FormData data
+ * @param contentType
+ * @param data
+ * @returns
+ */
+export function handlerFormDataBodyData(contentType: string, data: any) {
+  const boundary = contentType.replace(/^.*boundary=(.*)$/, '$1');
+  let str = '';
+  for (const [key, value] of Object.entries(data)) {
+    str += `--${boundary}\r\nContent-Disposition: form-data; name="${key}"\r\n\r\n${value}\r\n`;
+  }
+  return str;
+}
+
+/**
  * Fix proxied body if bodyParser is involved.
  */
 export function fixRequestBody(proxyReq: http.ClientRequest, req: Request): void {
@@ -18,15 +33,6 @@ export function fixRequestBody(proxyReq: http.ClientRequest, req: Request): void
     // deepcode ignore ContentLengthInCode: bodyParser fix
     proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
     proxyReq.write(bodyData);
-  };
-
-  const handlerFormDataBodyData = (contentType: string, data: any) => {
-    const boundary = contentType.replace(/^.*boundary=(.*)$/, '$1');
-    let str = '';
-    for (const [key, value] of Object.entries(data)) {
-      str += `--${boundary}\r\nContent-Disposition: form-data; name="${key}"\r\n\r\n${value}\r\n`;
-    }
-    return str;
   };
 
   if (contentType && contentType.includes('application/json')) {
--- test/unit/fix-request-body.spec.ts
+++ test/unit/fix-request-body.spec.ts
@@ -1,7 +1,7 @@
 import { ClientRequest } from 'http';
 import * as querystring from 'querystring';
-
-import { fixRequestBody } from '../../src/handlers/fix-request-body';
+import * as FormData from 'form-data';
+import { fixRequestBody, handlerFormDataBodyData } from '../../src/handlers/fix-request-body';
 import type * as express from 'express';
 import type { Request } from '../../src/types';
 
@@ -48,6 +48,40 @@ describe('fixRequestBody', () => {
     fixRequestBody(proxyRequest, { body: { someField: 'some value' } } as Request<express.Request>);
 
     const expectedBody = JSON.stringify({ someField: 'some value' });
+    expect(proxyRequest.setHeader).toHaveBeenCalledWith('Content-Length', expectedBody.length);
+    expect(proxyRequest.write).toHaveBeenCalledWith(expectedBody);
+  });
+
+  it('should write when body is not empty and Content-Type is multipart/form-data', () => {
+    const proxyRequest = fakeProxyRequest();
+    proxyRequest.setHeader('content-type', 'multipart/form-data');
+
+    jest.spyOn(proxyRequest, 'setHeader');
+    jest.spyOn(proxyRequest, 'write');
+
+    const body = new FormData();
+    body.append('someField', 'some value');
+
+    fixRequestBody(proxyRequest, { body } as Request<express.Request>);
+
+    const expectedBody = handlerFormDataBodyData('multipart/form-data', body);
+    expect(proxyRequest.setHeader).toHaveBeenCalledWith('Content-Length', expectedBody.length);
+    expect(proxyRequest.write).toHaveBeenCalledWith(expectedBody);
+  });
+
+  it('should write when body is not empty and Content-Type includes multipart/form-data', () => {
+    const proxyRequest = fakeProxyRequest();
+    proxyRequest.setHeader('content-type', 'multipart/form-data');
+
+    jest.spyOn(proxyRequest, 'setHeader');
+    jest.spyOn(proxyRequest, 'write');
+
+    const body = new FormData();
+    body.append('someField', 'some value');
+
+    fixRequestBody(proxyRequest, { body } as Request<express.Request>);
+
+    const expectedBody = handlerFormDataBodyData('multipart/form-data', body);
     expect(proxyRequest.setHeader).toHaveBeenCalledWith('Content-Length', expectedBody.length);
     expect(proxyRequest.write).toHaveBeenCalledWith(expectedBody);
   });
