c50dd06d9102fbb81dd4cbad7a295dddee5f6e1e
Steven Chim <655241+chimurai@users.noreply.github.com>
2025-04-09 20:26:28 +0200

    fix(fixRequestBody): handle invalid request (#1091)

--- src/handlers/fix-request-body.ts
+++ src/handlers/fix-request-body.ts
@@ -2,10 +2,20 @@ import type * as http from 'http';
 import type { Request } from '../types';
 import * as querystring from 'querystring';
 
+type HandleBadRequestArgs = {
+  proxyReq: http.ClientRequest;
+  req: http.IncomingMessage;
+  res: http.ServerResponse;
+};
+
 /**
  * Fix proxied body if bodyParser is involved.
  */
-export function fixRequestBody(proxyReq: http.ClientRequest, req: http.IncomingMessage): void {
+export function fixRequestBody(
+  proxyReq: http.ClientRequest,
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+): void {
   const requestBody = (req as Request).body;
 
   if (!requestBody) {
@@ -15,6 +25,18 @@ export function fixRequestBody(proxyReq: http.ClientRequest, req: http.IncomingM
   const contentType = proxyReq.getHeader('Content-Type') as string;
 
   if (!contentType) {
+    return;
+  }
+
+  // Handle bad request when unexpected "Connect: Upgrade" header is provided
+  if (/upgrade/gi.test(proxyReq.getHeader('Connection') as string)) {
+    handleBadRequest({ proxyReq, req, res });
+    return;
+  }
+
+  // Handle bad request when invalid request body is provided
+  if (hasInvalidKeys(requestBody)) {
+    handleBadRequest({ proxyReq, req, res });
     return;
   }
 
@@ -29,4 +51,15 @@ export function fixRequestBody(proxyReq: http.ClientRequest, req: http.IncomingM
   } else if (contentType.includes('application/x-www-form-urlencoded')) {
     writeBody(querystring.stringify(requestBody));
   }
+}
+
+function hasInvalidKeys(obj) {
+  return Object.keys(obj).some((key) => /[\n\r]/.test(key));
+}
+
+function handleBadRequest({ proxyReq, req, res }: HandleBadRequestArgs) {
+  res.writeHead(400);
+  res.end('Bad Request');
+  proxyReq.destroy();
+  req.destroy();
 }
