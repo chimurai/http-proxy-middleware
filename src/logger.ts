afcd576f69cb178ebb126655729dfdaec364e812
Mallikarjun-0 <saimallikarjun9@gmail.com>
2025-04-12 16:51:28 +0530

    fix: handle invalid `logger` in `getLogger`

--- src/logger.ts
+++ src/logger.ts
@@ -19,6 +19,29 @@ const noopLogger: Logger = {
   error: () => {},
 };
 
+function isLogger(obj: any): obj is Logger {
+  return (
+    obj &&
+    typeof obj === 'object' &&
+    typeof obj.info === 'function' &&
+    typeof obj.warn === 'function' &&
+    typeof obj.error === 'function'
+  );
+}
+
 export function getLogger(options: Options): Logger {
-  return (options.logger as Logger) || noopLogger;
+  const { logger } = options;
+
+  if (logger === undefined) {
+    return noopLogger;
+  }
+
+  if (!isLogger(logger)) {
+    console.warn(
+      '[http-proxy-middleware] Invalid logger provided. Falling back to default noopLogger.'
+    );
+    return noopLogger;
+  }
+
+  return logger;
 }
