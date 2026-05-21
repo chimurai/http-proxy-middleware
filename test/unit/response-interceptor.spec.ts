0353ea4015a711011ebbf3dd5dd364062c75aa70
Steven Chim <655241+chimurai@users.noreply.github.com>
2026-05-21 21:13:01 +0000

    fix(response-interceptor): reduce responseInterceptor buffer churn
    ---------
    
    Co-authored-by: Matt Fozard <matt@futureon.com>

--- test/unit/response-interceptor.spec.ts
+++ test/unit/response-interceptor.spec.ts
@@ -23,6 +23,48 @@ describe('responseInterceptor', () => {
     expect(res.end).toHaveBeenCalledWith();
   });
 
+  it('should combine proxy response chunks before calling interceptor', async () => {
+    const proxyRes = createMockRequest();
+    const req = createMockRequest();
+    const res = createMockResponse();
+
+    responseInterceptor(async (buffer) => {
+      expect(buffer).toEqual(Buffer.from('HPM'));
+      return buffer;
+    })(proxyRes, req, res);
+
+    proxyRes.emit('data', Buffer.from('H'));
+    proxyRes.emit('data', Buffer.from('P'));
+    proxyRes.emit('data', Buffer.from('M'));
+    proxyRes.emit('end');
+    await waitInterceptorHandler();
+
+    expect(res.setHeader).toHaveBeenCalledWith('content-length', 'HPM'.length);
+    expect(res.write).toHaveBeenCalledWith(Buffer.from('HPM'));
+    expect(res.end).toHaveBeenCalledWith();
+  });
+
+  it('should combine string proxy response chunks before calling interceptor', async () => {
+    const proxyRes = createMockRequest();
+    const req = createMockRequest();
+    const res = createMockResponse();
+
+    responseInterceptor(async (buffer) => {
+      expect(buffer).toEqual(Buffer.from('HPM'));
+      return buffer;
+    })(proxyRes, req, res);
+
+    proxyRes.emit('data', 'H');
+    proxyRes.emit('data', 'P');
+    proxyRes.emit('data', 'M');
+    proxyRes.emit('end');
+    await waitInterceptorHandler();
+
+    expect(res.setHeader).toHaveBeenCalledWith('content-length', 'HPM'.length);
+    expect(res.write).toHaveBeenCalledWith(Buffer.from('HPM'));
+    expect(res.end).toHaveBeenCalledWith();
+  });
+
   it('should end with error when receive a proxy error event', async () => {
     const proxyRes = createMockRequest();
     const req = createMockRequest();
