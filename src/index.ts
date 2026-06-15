438f6398d8b0dc243ce133592ae34de29121a040
Maikel van Dort <11695769+Netail@users.noreply.github.com>
2026-06-15 17:02:37 +0200

    Add OnProxyEvent type export to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from './factory.js';
 
 export * from './handlers/index.js';
 
-export type { Plugin, Filter, Options, RequestHandler } from './types.js';
+export type { Plugin, Filter, Options, RequestHandler, OnProxyEvent } from './types.js';
 
 export * from './plugins/index.js';
